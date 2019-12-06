fe229b6161be07ce1bc9c9cfbde59a43e7eefeec
mandrush <andruszkiewiczm1@gmail.com>
2019-12-06 16:30:16 +0100

    deletion fixup

--- src/main/scala/route/delete/DeletePostRoute.scala
+++ src/main/scala/route/delete/DeletePostRoute.scala
@@ -6,6 +6,7 @@ import akka.http.scaladsl.server.{MalformedFormFieldRejection, Route}
 import database.layer.DatabaseLayer
 import domain.PathNames.DeletePost
 import domain.logic.{FieldsValidation, ForumJSONSupport}
+import domain.rejection.ExceptionHandlers.databaseExceptionHandler
 import domain.request.UserRequests.Deletion
 import route.AppConfig
 import spray.json.JsValue
@@ -22,23 +23,25 @@ object DeletePostRoute extends ForumJSONSupport with FieldsValidation {
           if (!onlyContains(req, "secret")) {
             reject(MalformedFormFieldRejection("", s"Only secret field is allowed here"))
           } else {
-            entity(as[Deletion]) { delete =>
-              val maybePost = dbLayer.findPost(postId)
-              onComplete(maybePost) {
-                case Success(post) => post match {
-                  case Some(p) =>
-                    if (delete.secret == p.secret.value) {
-                      val deleteF = dbLayer.deletePost(postId)
-                      onComplete(deleteF) {
-                        case Success(_) => complete(HttpResponse(StatusCodes.OK, entity = s"Deleted post with id = $postId"))
-                        case Failure(e) => throw e
+            handleExceptions(databaseExceptionHandler) {
+              entity(as[Deletion]) { delete =>
+                val maybePost = dbLayer.findPost(postId)
+                onComplete(maybePost) {
+                  case Success(post) => post match {
+                    case Some(p) =>
+                      if (delete.secret == p.secret.value) {
+                        val deleteF = dbLayer.deletePost(postId)
+                        onComplete(deleteF) {
+                          case Success(_) => complete(HttpResponse(StatusCodes.OK, entity = s"Deleted post with id = $postId"))
+                          case Failure(e) => throw e
+                        }
+                      } else {
+                        complete(HttpResponse(StatusCodes.Unauthorized, entity = "Wrong secret"))
                       }
-                    } else {
-                      complete(HttpResponse(StatusCodes.Unauthorized, entity = "Wrong secret"))
-                    }
-                  case None    => complete(HttpResponse(StatusCodes.NotFound, entity = "There is no such post."))
+                    case None => complete(HttpResponse(StatusCodes.NotFound, entity = "There is no such post."))
+                  }
+                  case Failure(e) => throw e
                 }
-                case Failure(e) => throw e
               }
             }
           }
--- src/main/scala/route/delete/DeleteReplyRoute.scala
+++ src/main/scala/route/delete/DeleteReplyRoute.scala
@@ -6,6 +6,7 @@ import akka.http.scaladsl.server.{MalformedFormFieldRejection, Route}
 import database.layer.DatabaseLayer
 import domain.PathNames._
 import domain.logic.{FieldsValidation, ForumJSONSupport}
+import domain.rejection.ExceptionHandlers.databaseExceptionHandler
 import domain.request.UserRequests.Deletion
 import route.AppConfig
 import spray.json.JsValue
@@ -23,22 +24,24 @@ object DeleteReplyRoute extends ForumJSONSupport with FieldsValidation {
             reject(MalformedFormFieldRejection("", s"Only secret field is allowed here"))
           } else {
             entity(as[Deletion]) { delete =>
-              val maybeReply = dbLayer.findReply(replyId)
-              onComplete(maybeReply) {
-                case Success(reply) => reply match {
-                  case Some(r) =>
-                    if (delete.secret == r.secret.value) {
-                      val deleteF = dbLayer.deleteReply(replyId)
-                      onComplete(deleteF) {
-                        case Success(_) => complete(HttpResponse(StatusCodes.OK, entity = s"Deleted reply with id = $replyId"))
-                        case Failure(e) => throw e
+              handleExceptions(databaseExceptionHandler) {
+                val maybeReply = dbLayer.findReply(replyId)
+                onComplete(maybeReply) {
+                  case Success(reply) => reply match {
+                    case Some(r) =>
+                      if (delete.secret == r.secret.value) {
+                        val deleteF = dbLayer.deleteReply(replyId)
+                        onComplete(deleteF) {
+                          case Success(_) => complete(HttpResponse(StatusCodes.OK, entity = s"Deleted reply with id = $replyId"))
+                          case Failure(e) => throw e
+                        }
+                      } else {
+                        complete(HttpResponse(StatusCodes.Unauthorized, entity = "Wrong secret"))
                       }
-                    } else {
-                      complete(HttpResponse(StatusCodes.Unauthorized, entity = "Wrong secret"))
-                    }
-                  case None => complete(HttpResponse(StatusCodes.NotFound, entity = "There is no such reply."))
+                    case None => complete(HttpResponse(StatusCodes.NotFound, entity = "There is no such reply."))
+                  }
+                  case Failure(e) => throw e
                 }
-                case Failure(e) => throw e
               }
             }
           }
